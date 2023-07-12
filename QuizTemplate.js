98a7c297e6b78801752a41250685a86d6173e79c
Joseph Bralic <joseph.bralic@gmail.com>
2023-07-12 23:21:37 +0930

    started wireframe

---
+++ QuizTemplate.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+        question : 'In which year was The Godfather first released?',
+        array: ["1969","1972","1973"], 
+        // A : '1969',
+        // B : '1972',
+        // C : '1973'
+        correct: "1972"
+    },
+    {
+        question : 'Which former Doctor Who actress played the part of Nebula in Avengers:Infinity War?',
+        array: ["Billie Piper","Jenna Coleman",'Karen Gillan'],
+        // A : 'Billie Piper',
+        // B : 'Jenna Coleman',
+        // C : 'Karen Gillan'
+        correct: "Karen Gillan"
+    },
+    {
+        question : 'Which film starring Chris Hemsworth and Daniel Brühl charts the Formula 1 rivalry of James Hunt and Niki Lauda?',
+        array: ['Fast and Furious','Rush','Ford V Ferrari'],
+        // A : 'Fast and Furious',
+        // B : 'Rush',
+        // C : 'Ford V Ferrari'
+        correct: "Rush"
+    }
+]
